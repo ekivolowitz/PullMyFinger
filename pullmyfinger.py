8216021c079ae2f9d745ff04201c7e8064f64d25
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-12-01 19:24:55 -0600

    added -l functionality.

--- pullmyfinger.py
+++ pullmyfinger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 from github import Github
 from github.GithubException import UnknownObjectException
 import os
@@ -6,22 +7,42 @@ import sys
 import argparse
 import getpass
 from pprint import pprint
+from consts import pull_my_finger, displaying
 parser = argparse.ArgumentParser()
 
+def ls(org, org_name):
+    projects = "\n"
+    for project in org.get_repos():
+        projects += project.name + "\n"
+    print(displaying.format(org_name, projects))
+ 
+def clone(org, organization_name, username, password):
+    for project in org.get_repos():
+        repo = project._rawData['html_url']
+        location = organization_name + os.sep + project.name
+        domain = repo.split("https://")[1]
+        url = "https://{}:{}@{}".format(username, password, domain)
+        os.system("git clone {} {}".format(url, location))
+
 if __name__ == "__main__":
+    print(pull_my_finger)
+
     parser.add_argument("organization", help="List the name of an organization that you'd like to pull down.")
+    parser.add_argument("-l", "--list", action="store_true", help="List the repositories in the organization.")
     args = parser.parse_args()
+
     username = input("Username: ")
     password = getpass.getpass("password: ")
+
     g = Github(username, password)
+
     try:
         org = g.get_organization(args.organization)
     except UnknownObjectException as e:
         print("Organization: '{}' does not exist. Please enter a valid organization name.".format(args.organization))
         sys.exit(1)
-    for project in org.get_repos():
-        repo = project._rawData['html_url']
-        location = args.organization + os.sep + project.name
-        domain = repo.split("https://")[1]
-        url = "https://{}:{}@{}".format(username, password, domain)
-        os.system("git clone {} {}".format(url, location))
+
+    if args.list:
+        ls(org, args.organization)
+    else:
+        clone(org, args.organization, username, password)
