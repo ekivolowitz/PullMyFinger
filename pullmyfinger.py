124c42e1cd9115738dfc43e49c979abd644626cd
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-12-23 18:46:56 -0600

    Added -L

--- pullmyfinger.py
+++ pullmyfinger.py
@@ -3,6 +3,7 @@
 # Date: 12-01-2019
 # Description: A tool to help manage a Github Organization
 # License: See Github Repository, GNU GPL 3.0
+# Last used error code: 104
 from github import Github
 from github.GithubException import UnknownObjectException, BadCredentialsException
 import os
@@ -11,8 +12,48 @@ import argparse
 from pprint import pprint
 from consts import pull_my_finger, displaying
 import json
+
+PAGE_SIZE = 30
 parser = argparse.ArgumentParser()
 
+def check_in_history(cursors, sha):
+    for commit in cursors:
+        if sha == commit.sha:
+            return True
+    return False
+
+def get_commit_history(repo):
+    return repo.get_commits()
+
+def get_repo_head_local(relative_path, repo_name):
+    '''
+        file containing the head - .git/refs/heads/master
+    '''
+    path = os.path.expanduser(relative_path + os.sep + repo_name)
+    master_path = '.git/refs/heads/master'
+    try:
+        with open(path + os.sep + master_path, 'r') as f:
+            return f.read().strip()
+    except:
+        print("ERROR: Cannot read master head ref file for {}{}{}".format(relative_path, os.sep, repo_name))
+        sys.exit(104)
+
+def diff_ls(org, path):
+    print()
+    for project in org.get_repos():
+        local_head = get_repo_head_local(path, project.name)
+        commits = get_commit_history(project)
+
+        last_page = int(commits.totalCount / PAGE_SIZE)
+        lp = commits.get_page(0)
+        github_most_recent_commit_sha = lp[0]
+        if local_head == github_most_recent_commit_sha.sha:
+            print("%-30s: remote equal to local." % (org.login + "/" + project.name))
+            continue
+        elif check_in_history(commits, local_head):
+            print("%-30s: remote ahead of local" % (org.login + "/" +  project.name))
+        else:
+            print("%-30s: local ahead of remote" %(org.login + "/" + project.name))
 def ls(org):
     '''
     ls - Lists all the repository names in an organization
@@ -61,7 +102,7 @@ if __name__ == "__main__":
             For example, `pullmyfinger -lq organization` would only print the \
             names of the repositories within `organization`.")
     parser.add_argument("-l", "--list", action="store_true", help="List the repositories in the organization.")
-    parser.add_argument("-L", action="store_true", help="Display repositories within organization that contain\
+    parser.add_argument("-L", help="Path to an organization folder locally. Display repositories within organization that contain\
             commits that have a HEAD on a different commit.")
     args = parser.parse_args()
 
@@ -92,6 +133,6 @@ if __name__ == "__main__":
         else:
             print(displaying.format(args.organization, formatted_projects))
     elif args.L:
-        print("Coming soon!")
+        diff_ls(org, args.L)
     else:
         clone(org, args.organization, username, token)
